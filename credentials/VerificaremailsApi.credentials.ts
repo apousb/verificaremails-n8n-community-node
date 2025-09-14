21c72143ac6bacaa31a3872dc3d0cf92f499ab0b
apousb <antxon.pous@newslettersoft.com>
2025-09-14 16:53:23 +0200

    Update VerificaremailsApi.credentials.ts

--- credentials/VerificaremailsApi.credentials.ts
+++ credentials/VerificaremailsApi.credentials.ts
@@ -1,7 +1,7 @@
 export class VerificaremailsApi {
   name = 'verificaremailsApi';
   displayName = 'Verificaremails API';
-  icon = 'file:nodes/Verificaremails/verificaremails.svg';
+  icon = 'file:verificaremails.svg';
   documentationUrl = 'https://www.verificaremails.com/docs/';
   properties = [
     { displayName: 'API Key', name: 'apiKey', type: 'string', default: '' },

634cfad56661c0bb0eab211ecbd601b7c4650198
apousb <antxon.pous@newslettersoft.com>
2025-09-18 15:06:44 +0200

    Update Verificaremails.node.ts

--- src/nodes/Verificaremails/Verificaremails.node.ts
+++ src/nodes/Verificaremails/Verificaremails.node.ts
@@ -4,7 +4,7 @@ export class Verificaremails {
   description = {
     displayName: 'VerificarEmails',
     name: 'verificaremails',
-    icon: 'file:nodes/Verificaremails/verificaremails.svg',
+    icon: 'file:verificaremails.svg',
     group: ['transform'],
     version: 1,
     description: 'Validate emails, phones, names and postal addresses using VerificarEmails.com API',

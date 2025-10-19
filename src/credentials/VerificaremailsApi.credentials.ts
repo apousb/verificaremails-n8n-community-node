b508364bfcc46ba63563c0b653e5e49c3ae77f08
apousb <antxon.pous@newslettersoft.com>
2025-10-19 14:06:32 +0200

    fix
    fix

--- src/credentials/VerificaremailsApi.credentials.ts
+++ src/credentials/VerificaremailsApi.credentials.ts
@@ -1,9 +1,16 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
+import {
+	ICredentialType,
+	INodeProperties,
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	IHttpRequestMethods,
+} from 'n8n-workflow';
 
 export class VerificaremailsApi implements ICredentialType {
 	name = 'verificaremailsApi';
 	displayName = 'VerificarEmails.com API';
 	documentationUrl = 'https://www.verificaremails.com/docs/';
+
 	properties: INodeProperties[] = [
 		{
 			displayName: 'API Key',
@@ -15,9 +22,8 @@ export class VerificaremailsApi implements ICredentialType {
 		},
 	];
 
-	// (Opcional pero recomendado) añade el token a todas las requests del nodo
-	// para no repetirlo en cada operación.
-	authenticate = {
+	// Añade ?auth-token=<API_KEY> a todas las requests
+	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
 			qs: {
